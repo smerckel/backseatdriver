aab509113a7af7c1b1652449dada170ae495fabc
Lucas Merckelbach <lucas.merckelbach@hereon.de>
2025-07-08 07:49:24 +0200

    Working version with mini pro.
    It seems that the softwareSerial is not reliable when it comes
    tp receiving data from the serial port. Hardware serial works.

--- arduino/max3232_test/max3232_test.ino
+++ arduino/max3232_test/max3232_test.ino
@@ -1,32 +1,27 @@
-#include<SoftwareSerial.h>
+//#include<SoftwareSerial.h>
 
-SoftwareSerial serial=SoftwareSerial(2,3);
-
+const uint8_t rxPin=10;
+const uint8_t txPin=11;
+//SoftwareSerial serial=SoftwareSerial(rxPin, txPin);
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communication with the Arduino (TTL)
+  Serial.begin(9600);
   Serial.println("MAX3232 RS232-to-TTL Test");
-  serial.begin(9600);
   pinMode(13, OUTPUT);
-
 }
 
 void loop() {
   // Check if data is available from the RS232 device
-  if (serial.available()) {
+  while (Serial.available()) {
     char data = Serial.read(); // Read data from the RS232 device
     Serial.print("Received: ");
-    Serial.println(data); // Print the received data to the Serial Monitor
-
-    // Echo the data back to the RS232 device
-    serial.write(data);
+    Serial.println(data); // Print the received data to the serial Monitor
   }
   digitalWrite(13, HIGH);
-  serial.write("hello\r\n");
-  Serial.write("hello\r\n");
-  delay(1000);
+  Serial.println("loop");
+  delay(500);
   digitalWrite(13, LOW);
-  delay(1000);
+  delay(500);
 }
 
 
